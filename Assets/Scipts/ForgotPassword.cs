0ca9e8f03e79ad09a9ecab1ce706c78354016ae9
luanpc <luanpc2805@gmail.com>
2023-11-18 18:01:31 +0700

    reset password

--- Assets/Scipts/ForgotPassword.cs
+++ Assets/Scipts/ForgotPassword.cs
@@ -7,7 +7,8 @@ using System.Text;
 using TMPro;
 public class ForgotPassword : MonoBehaviour
 {
-    public TMP_InputField txtUser;
+    public TMP_InputField txtUser, txtOTP, txtNewPass, txtRenewPass;
+    public GameObject resetPassword, sendOTP, login;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +28,7 @@ public class ForgotPassword : MonoBehaviour
         SendOTPAPI(oTPModel);
     }
 
+    //Goi API send OTP
     IEnumerator SendOTPAPI(OTPModel otpModel)
     {
         string jsonStringRequest = JsonConvert.SerializeObject(otpModel);
@@ -47,6 +49,56 @@ public class ForgotPassword : MonoBehaviour
             if (responseModel.status == 1)
             {
                 // load panel reset
+                resetPassword.SetActive(true);
+                sendOTP.SetActive(false);
+            }
+            else
+            {
+                // hien thi thong bao
+            }
+        }
+    }
+
+    public void ResetPass()
+    {
+        var newPass = txtNewPass.text;
+        var renewPass = txtRenewPass.text;
+        if (newPass.Equals(renewPass))
+        {
+            var user = txtUser.text;
+            int otp = int.Parse(txtOTP.text);
+            ResetPassModel resetPassModel = new ResetPassModel(user, otp, newPass);
+            StartCoroutine(ResetPassAPI(resetPassModel));
+            ResetPassAPI(resetPassModel);
+        }
+        else
+        {
+            //hien thi thong bao
+        }
+    }
+    //Goi API reset pass
+    IEnumerator ResetPassAPI(ResetPassModel resetPassModel)
+    {
+        string jsonStringRequest = JsonConvert.SerializeObject(resetPassModel);
+        var request = new UnityWebRequest("https://hoccungminh.dinhnt.com/fpt/reset-password", "POST");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonStringRequest);
+        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        yield return request.SendWebRequest();
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            var jsonString = request.downloadHandler.text.ToString();
+            ResponseModel responseModel = JsonConvert.DeserializeObject<ResponseModel>(jsonString);
+            if (responseModel.status == 1)
+            {
+                // return login / main menu
+                resetPassword.SetActive(false);
+                login.SetActive(true);
             }
             else
             {
